1fc874f3e22cb990bd441abd8c0cc7f2f932591d
Connor Dinnadge <cdinnadge97@gmail.com>
2024-07-29 22:01:31 +0100

    setup basic main func to load envs and connect to db

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fitness-tracker-api/database"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load the environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Connect to the database
+	database.Connect()
+}
